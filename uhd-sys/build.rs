b6e9c5c96864693f12f3163763d61aa360abf470
Samuel Craig Carey <sccarey@sandia.gov>
2023-01-26 13:07:22 -0700

    add env path feature

--- uhd-sys/build.rs
+++ uhd-sys/build.rs
@@ -5,14 +5,34 @@ use std::env;
 use std::path::{Path, PathBuf};
 
 fn main() {
-    println!(r"cargo:rustc-link-search=C:\Users\groundstation\projects\assets\lib");
-    // println!("cargo:rustc-link-lib=gcc");
     println!("cargo:rustc-link-lib=uhd");
+    println!("cargo:rustc-link-lib=boost_chrono");
+    println!("cargo:rustc-link-lib=boost_date_time");
+    println!("cargo:rustc-link-lib=boost_filesystem");
+    println!("cargo:rustc-link-lib=boost_regex");
+    println!("cargo:rustc-link-lib=boost_system");
+    println!("cargo:rustc-link-lib=boost_thread");
+    println!("cargo:rustc-link-lib=c");
+    println!("cargo:rustc-link-lib=erio");
+    // println!("cargo:rustc-link-lib=gcc_s"); // Doesn't seem to be needed, but I had this for some reason earlier...
+    println!("cargo:rustc-link-lib=stdc++");
+    println!("cargo:rustc-link-lib=udev");
 
-    let _ = dotenv::dotenv();
-    let uhd_include_path = if let Ok(path) = env::var("UHD_INCLUDE_DIR") {
+    dotenv::dotenv().ok();
+    let uhd_include_path = if let Ok(uhd_path) = env::var("UHD_DIR") {
         // Use the path provided via environmental variable
-        Path::new(&path).to_owned()
+
+        let uhd_path = Path::new(&uhd_path);
+        let this_crate_path = env::var("CARGO_MANIFEST_DIR").unwrap();
+        println!(
+            r"cargo:rustc-link-search={}",
+            Path::new(&this_crate_path)
+                .join(uhd_path)
+                .join("lib")
+                .to_str()
+                .unwrap()
+        );
+        uhd_path.to_owned().join("include")
     } else {
         // This reads the metadata in Cargo.toml and sends Cargo the appropriate output to link the
         // libraries
